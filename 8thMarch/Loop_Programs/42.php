996250fa37d9bf1d526d253abad3174c1a99ad9b
jinal-addweb <jinal.s.addweb@gmail.com>
2022-03-29 19:41:35 +0530

    Array Programs

--- 8thMarch/Loop_Programs/42.php
+++
@@ -1,8 +0,0 @@
-<?php
-$n=$_POST['first'];
-for($i=1;$i<=$n;$i+=3)
-{
-    echo $i;
-    echo "<br>";
-}
-?>
--- 8thMarch/Loop_Programs/59.php
+++
@@ -1,7 +0,0 @@
-<?php
-for($i=65;$i<=122;$i++)
-{
-    echo "ASCII value of ".chr($i)." is: ".$i;
-    echo "<br><br>";
-}
-?>
